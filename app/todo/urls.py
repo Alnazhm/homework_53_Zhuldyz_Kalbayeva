245653a8a377ab0cc8d81be0806bd1df8c3935bf
Zhuldyz Kalbayeva <kalbayevazh@gmail.com>
2022-09-23 08:16:12 +0600

    Добавила функцию удаления на главной странице

--- app/todo/urls.py
+++ app/todo/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from todo.views.base import index_view
-from todo.views.create_task import add_view, detail_view
+from todo.views.create_task import add_view, detail_view, delete_task_view
+
+
 urlpatterns = [
     path('', index_view, name='index'),
     path('tasks/add/', add_view, name='task_add'),
-    path('tasks/<int:pk>', detail_view, name='task_detail')
+    path('tasks/<int:pk>', detail_view, name='task_detail'),
+    path('tasks/del/<int:pk>', delete_task_view, name='task_deleted')
 ]
--- app/todo/views/create_task.py
+++ app/todo/views/create_task.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, redirect, get_object_or_404
 from todo.models import Todo, STATUS_OF_TASK
 
 
-def add_view(request):
+def add_view(request, *args, **kwargs):
     if request.method == 'GET':
         context = {
             'status_list': STATUS_OF_TASK
@@ -12,11 +14,25 @@ def add_view(request):
     task_data = {
         'description': request.POST.get('description'),
         'status': request.POST.get('status'),
-        'lead_at': request.POST.get('lead_at')
+        'lead_at': request.POST.get('lead_at'),
+        'detail_description': request.POST.get('detail_description')
     }
     task = Todo.objects.create(**task_data)
     return redirect('task_detail', pk=task.pk)
 
+
 def detail_view(request, pk):
     todo_task = get_object_or_404(Todo, pk=pk)
     return render(request, 'task.html', context={'todo_task': todo_task})
+
+def delete_task_view(request, pk):
+    todo_task = get_object_or_404(Todo, pk=pk)
+    todo_task.delete()
+    return render(request, 'task_delete.html', context={'todo_task': todo_task})
+
+
+
+
+
+
+
